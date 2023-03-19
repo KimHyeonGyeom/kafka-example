eaf7a8af961d88b478ba4e383a8e776d0a35d48c
앵갬 <couse6610@gmail.com>
2023-03-19 22:50:38 +0900

    fix: RequestLoggingMiddleware를 빼고 kafka controller 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { KafkaModule } from './kafka/kafka.module';
 import { KafkaProducerService } from './kafka/kafka-producer.service';
 import { KafkaController } from './kafka/kafka.controller';
 import { KafkaConsumerService } from './kafka/kafka-consumer.service';
-import { RequestLoggingMiddleware } from './middleware/RequestLogging.middleware';
 
 @Module({
   imports: [KafkaModule],
   controllers: [AppController, KafkaController],
   providers: [AppService, KafkaProducerService, KafkaConsumerService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
--- src/kafka/kafka.controller.ts
+++ src/kafka/kafka.controller.ts
@@ -1,18 +1,25 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
 import { KafkaProducerService } from './kafka-producer.service';
-import { KAFKA_TOPIC } from '../constants/kafka.constants';
 
 @Controller('request')
 export class KafkaController {
   constructor(private readonly kafkaProducerService: KafkaProducerService) {}
 
-  @Post()
-  async handleRequest(@Body() requestBody: any): Promise<string> {
-    const topic = KAFKA_TOPIC;
-    const key = requestBody.key;
-    const value = JSON.stringify(requestBody);
+  @Get()
+  async handleRequest(@Request() req): Promise<string> {
+    const requestInfo = {
+      method: req.method,
+      url: req.originalUrl,
+      headers: req.headers,
+      body: req.body,
+      param: req.params,
+      timestamp: new Date(),
+    };
 
-    await this.kafkaProducerService.sendMessage(topic, key, value);
+    const key = 'request-info';
+    const value = JSON.stringify(requestInfo);
+    await this.kafkaProducerService.sendMessage('topic', key, value);
+
     return 'Message sent to Kafka successfully!';
   }
 }
